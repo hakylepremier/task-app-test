bc2475f8b021b3185c5b2eca72dab616afa622d9
Humphrey Yeboah <haky@haky.com>
2024-04-10 19:52:10 +0000

    feat: rename dashboard page to task page

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::view('tasks', 'tasks')
+    ->middleware(['auth'])
+    ->name('tasks');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
